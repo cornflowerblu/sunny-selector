a22628d441f8c12bd7e1964e89108f7139c2501c
Roger Urich <rogerdurich+git@gmail.com>
2022-11-03 05:56:41 -0700

    update tests

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -3,11 +3,12 @@ describe("main page", () => {
     cy.visit("localhost:3000");
   });
 
-  it("shuffles a few episodes, generates a cookie, and can fetch details", () => {
+  it("shuffles a few episodes and can show/hide details", () => {
     cy.visit("localhost:3000");
     cy.contains("Shuffle").click();
     cy.contains("Shuffle").click();
     cy.contains("Shuffle").click();
     cy.contains("Details").click().document().get("h3").should("exist");
+    cy.contains("Details").click().document().get("h3").should("not.exist");
   });
 });
