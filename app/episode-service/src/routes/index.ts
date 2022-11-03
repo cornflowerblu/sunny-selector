d5f1f75714fdf77c135dbffb2ac35a29b237b27e
Roger Urich <rogerdurich+git@gmail.com>
2022-11-03 04:34:40 -0700

    added scripts to start

--- app/episode-service/src/routes/index.ts
+++ app/episode-service/src/routes/index.ts
@@ -2,8 +2,8 @@ import express, { Request, Response } from 'express'
 
 const router = express.Router()
 
-router.get('/', (req: Request, res: Response) => {
-  res.send('Hello World!')
+router.get('/hello', (req: Request, res: Response) => {
+  res.send('Hello World!').status(200)
 })
 
 module.exports = router
