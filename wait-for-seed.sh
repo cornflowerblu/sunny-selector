98ab8e5b02de056f2bca17e6da7731c58c4d7ba9
Roger Urich <rogerdurich+git@gmail.com>
2022-11-03 06:33:20 -0700

    two hopefully helpful changes

--- wait-for-seed.sh
+++ wait-for-seed.sh
@@ -1,6 +1,12 @@
-CODE= docker wait selector-graphql-cli-1
+LOCAL_CODE= docker wait selector-graphql-cli-1
+REMOTE_CODE= docker wait sunny-selector-graphql-cli-1
 
-while [ ! $CODE = 0 ]; do
+while [ ! $LOCAL_CODE = 0 ]; do
+  echo "Waiting for seeding to complete..."
+  sleep 30
+done
+
+while [ ! $REMOTE_CODE = 0 ]; do
   echo "Waiting for seeding to complete..."
   sleep 30
 done
